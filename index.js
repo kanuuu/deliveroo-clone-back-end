08c8492fa6d222aa03bfc78ee21eada90f2b1de2
kanuuu <yuuniichi@gmail.com>
2022-11-09 14:25:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const data = require("./data.json");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+app.all("*", (req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+app.listen(4000, (req, res) => {
+  console.log("Server started");
+});
